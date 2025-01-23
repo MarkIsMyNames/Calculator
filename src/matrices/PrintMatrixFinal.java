ff3abb764a3ada4ed0c977156292c9032220ac8d
bob <mrakemail@proton.me>
2025-01-23 10:39:10 +0000

    Minor improvements to InputProcessor alot more work needed

--- src/matrices/PrintMatrixFinal.java
+++ src/matrices/PrintMatrixFinal.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
 
 public interface PrintMatrixFinal extends PrintMatrixSuper {
     public static void printFinal(double[][] matrixToPrint){
+
         System.out.println("\n\nYour Final Matrix: ");
         PrintMatrixSuper.printMatrix(matrixToPrint);
         Scanner scanner = new Scanner(System.in);
+
+        //To Return to the main menu
         scanner.nextLine();
         MenuManager.clearScreen();
         MatricesManager.start();
